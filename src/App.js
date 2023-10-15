8216bc8306756f6821008b0aa5ed7216b7fb6ff8
Anwar-Barakat <brkatanwar0@gmail.com>
2023-10-15 12:07:30 +0300

    adding the navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import "./App.css";
+import Main from "./components/Main/Main";
 
 function App() {
   return (
     <div className="App">
-      <h1 className="text-3xl font-bold underline text-red-500">
-        Hello world!
-      </h1>
+      <Main>
+        
+      </Main>
     </div>
   );
 }
